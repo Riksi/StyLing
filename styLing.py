389da809232c801ef4bcb60b37751a8dc227691c
User <user@Users-MacBook-Pro-2.local>
2017-08-11 19:26:49 +0100

    First commit

---
+++ styLing.py
@@ -0,0 +1,109 @@
+from flask import Flask, request, session, url_for, redirect, \
+     render_template, abort, g, flash, _app_ctx_stack
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.layers import LSTM, Embedding
+from keras.optimizers import RMSprop, Adam
+import pickle
+from collections import Counter
+
+
+app = Flask(__name__)
+
+MAX_LEN = 30 
+EMBED_DIM = 300
+PERPLEXITY = 152
+WIDTHS = [0,25,75,100]
+VOCAB_SIZE = 10000
+
+
+
+all_vectors = pickle.load(open('/users/user/eecs/Deep_Learning/NLP/fastTextHindi.vec','rb'))
+
+vec_dict = pickle.load(open('vec_pickle.pkl','rb'))
+
+word_indices = vec_dict['word_indices']
+
+indices_word = vec_dict['indices_word']
+
+vector_rows = vec_dict['vector_rows']
+
+vectors = all_vectors[vector_rows]
+vectors = np.concatenate([np.zeros(vectors[0:1].shape), vectors],axis=0)
+
+del all_vectors
+
+
+print("word_indices", type(word_indices), "length:",len(word_indices) )
+print("indices_words", type(indices_word), "length", len(indices_word))
+
+print('Num vectors', len(vectors))
+
+def get_word_vec(word):
+    return vectors[word_indices[word]]
+
+#Using equations 8 and 9 in http://web.stanford.edu/class/cs224n/lecture_notes/cs224n-2017-notes5.pdf
+#for 'corpuses' of sizes 1 to length of input text
+
+
+#The idea is that our model is trained on the 'right' corpus so a low
+#perplexity would suggest that this corpus is right whilst a high one would suggest
+#otherwise
+
+def build_model():
+    model = Sequential()
+    model.add(LSTM(512, return_sequences=True, input_shape=(MAX_LEN, EMBED_DIM)) )
+    model.add(Dropout(0.2))
+    model.add(LSTM(512, return_sequences=False))
+    model.add(Dropout(0.2))
+    model.add(Dense(VOCAB_SIZE))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy',optimizer=RMSprop())
+    model.load_weights('weights_hindi_epoch_11.pkl')
+    return model
+
+
+def get_metrics(tokens):
+    model = build_model()
+    sent = [get_word_vec(token) for token in tokens[:30]]
+    losses = []
+    probs = []
+    for token in tokens[30:]:
+        out = np.zeros((1, VOCAB_SIZE), dtype=np.int32)
+        ind = word_indices[token]
+        out[0,ind] = 1
+        x = np.expand_dims(sent,axis=0)
+        loss = model.evaluate(x,out,verbose=False)
+        preds = model.predict(x,verbose=False)
+        probs.append(preds[0, ind])
+        sent.append(get_word_vec(token))
+        del sent[0]
+        if len(losses) > 0:
+            loss = losses[-1]+loss
+        losses.append(loss)
+    perplexities = np.exp(losses/np.arange(1,len(losses)+1))
+    return probs, perplexities
+
+
+@app.route('/', methods=['GET'])
+def display():
+    return render_template('styLing.html')
+
+@app.route('/', methods=['POST'])
+def evaluate():
+    text = request.form.get('text')
+    # p,q = get_metrics(tokens)
+    tokens = text.strip().split()
+    if (len(tokens) < 100):
+        return render_template('styLing.html', message = 'Please enter at least 100 words')
+    pr,px = get_metrics(tokens)
+    width = 100*PERPLEXITY/float(px[-1])
+    print(px[-1],width)
+    probs = {'tokens':tokens, 'probs':[float(i) for i in pr]}
+    a = 0.00001
+    return render_template('styLing.html', a = a, text = text, width=width, probs=probs)
+
+
+app.debug = True
+app.run(port=5555, host='0.0.0.0')
